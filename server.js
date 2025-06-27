d5907825e94d47acd9831272e52067ad29ceeec2
neojarvis <techadmin@iamneo.ai>
2025-06-27 10:23:40 +0000

    Submit-project-save-commit-27/06/2025--15:53:35

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8081;
+console.log("HH")
 
 //app.use('/static', express.static('public'))
 app.use(express.static('public'))
